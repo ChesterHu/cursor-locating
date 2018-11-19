ad8b51afc06a3d6370086452b3b26888668f41ec
Chufeng <huchufeng@g.ucla.edu>
2018-11-19 16:46:53 -0500

    init config for webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, 'client', 'src');
+const DIST_DIR = path.join(__dirname, 'client', 'dist');
+
+module.exports = {
+	entry: path.join(SRC_DIR, 'index.js'),
+	output: {
+		path: DIST_DIR,
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['.js', '.css', '.json']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader'
+			},
+			{
+        test: /\.png$/,
+        loader: 'url-loader?limit=100000&minetype=image/png'
+      },
+      {
+        test: /\.jpg$/,
+        loader: 'file-loader'
+			},
+			{
+        test : /\.js$/,
+        include : SRC_DIR,
+        loader : 'babel-loader',
+      }
+		],
+	},
+	plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
+};
