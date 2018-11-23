cb237152ae17b066b859a3c733040d1716d0dbad
Chufeng <huchufeng@g.ucla.edu>
2018-11-23 11:49:37 -0500

    include ripples

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/app';
+
 ReactDOM.render(
 	<div> 
-		Hello, World! 
+		<App />
 	</div>,
 	document.getElementById('root'));
