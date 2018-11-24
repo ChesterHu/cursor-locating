d52cd9740c682fa5d11dccbcd2745f18a9463834
Chufeng <huchufeng@g.ucla.edu>
2018-11-24 17:23:38 -0500

    add image as background

--- client/src/containers/task_detail.js
+++ client/src/containers/task_detail.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Target from '../components/target';
+import BackGroundImg from '../resource/img/1.jpg';
+
+const toCSS = () => {
+	return {
+		height: '800px',
+		backgroundImage: `url(${BackGroundImg})`
+	}
+}
 
 class TaskDetail extends Component {
 	render() {
 		console.log(this.props.task);
 		return (
-			<div>
+			<div style={toCSS()}>
 				TaskDetail
 				<Target />
 			</div>
