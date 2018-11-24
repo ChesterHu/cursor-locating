96672743d4f0bc7cc5a36482c74604193ab11b81
Chufeng <huchufeng@g.ucla.edu>
2018-11-24 12:08:35 -0500

    move index.html into client/dist

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static(`${__dirname}/../client/dist`));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(`${__dirname}/../index.html`));
+	res.sendFile(path.resolve(`${__dirname}/../client/dist/index.html`));
 });
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}!`);
