c04fb51f6514adb1abe87a459f2966ea4cb6405f
Chufeng <huchufeng@g.ucla.edu>
2018-11-24 12:24:12 -0500

    init reducers

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import App from './components/app';
+import reducer from './reducers/index';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
 	<div> 
