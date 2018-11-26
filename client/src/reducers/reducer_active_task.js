a9ab83588acbb281aad2816764a611dbee5a869e
Chufeng <huchufeng@g.ucla.edu>
2018-11-25 22:24:46 -0500

    change image import methods

--- client/src/reducers/reducer_active_task.js
+++ client/src/reducers/reducer_active_task.js
@@ -1,3 +1,8 @@
+import * as actionType from '../actions/constants';
+
 export default function(state = 0, action) {
+	if (action.type == actionType.CLICK_TARGET) {
+		console.log('here');
+	}
 	return state;
 }
