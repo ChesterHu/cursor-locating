27dd1e3bfe88d879fbc54d64a9ed2a3599e299f6
Chufeng <huchufeng@g.ucla.edu>
2018-11-27 22:47:07 -0500

    remove boundary

--- client/src/reducers/reducer_active_task.js
+++ client/src/reducers/reducer_active_task.js
@@ -2,8 +2,7 @@ import * as actionType from '../actions/constants';
 
 export default function(state = 0, action) {
 	if (action.type == actionType.CLICK_TARGET) {
-		if (state < actionType.NUM_TASKS - 1)
-			state = state + 1;
+		state = state + 1;
 	}
 	return state;
 }
