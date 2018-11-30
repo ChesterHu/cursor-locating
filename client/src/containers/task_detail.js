eca19a5aa9a3521edef64313033f6b0d57e237ad
Chufeng <huchufeng@g.ucla.edu>
2018-11-29 23:23:06 -0500

    test screen size

--- client/src/containers/task_detail.js
+++ client/src/containers/task_detail.js
@@ -17,10 +17,8 @@ const RECORD_TIME = 100;
 
 const toCSS = (imgUrl) => {
 	return {
-		width:'100%',
-		height:'100%',
-		top: 0,
-		left: 0,
+		width: '100%',
+		height: '100%',
 		position: 'absolute',
 		backgroundImage: `url(${imgUrl})`,
 		backgroundSize: 'cover',
