a290cdd57f82cd0b553e57648a5fd23828348211
Chufeng <huchufeng@g.ucla.edu>
2018-11-29 19:21:53 -0500

    feat: display task and questionnaire depends on task finished

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Ripples from './ripples';
 import TaskList from '../containers/task_list';
 import TaskDetail from '../containers/task_detail';
 import Questionnaire from '../containers/questionnaire';
 
-export default function() {
-	return (
-		<Questionnaire />
-	);
+function App(props) {
+	return props.taskComplete ? <Questionnaire /> : <TaskDetail />
 }
+
+function mapStateToProps({ tasks, activeTask }) {
+	return ({
+		taskComplete: activeTask >= tasks.length
+	});
+}
+
+export default connect(mapStateToProps)(App);
