5b3e752db4bb7521b0c74cc55257a744de0be9ef
Yu Gu <crispherg@gmail.com>
2018-11-30 00:29:20 -0500

    post receiver write folder

--- client/dist/post_receiver.php
+++ client/dist/post_receiver.php
@@ -1,11 +1,20 @@
 <html>
     <body>
         <?php
+
+$dir = 'log2';
+
+// create new directory with 744 permissions if it does not exist yet
+// owner will be the user/group the PHP script is run under
+if ( !file_exists($dir) ) {
+    mkdir ($dir, 0744);
+}
+
         $requestBody = file_get_contents('php://input');
         $dec = json_decode($requestBody) or die ("could not decode json.");
         $dict = $dec->{'userInfo'};
         $filename = $dict->name;
-        file_put_contents('log/' . $filename . '.log', file_get_contents('php://input'));
+        file_put_contents($dir . $filename . '.log', file_get_contents('php://input'));
         ?>
     </body>
 </html>
