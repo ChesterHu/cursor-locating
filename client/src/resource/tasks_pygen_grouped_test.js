05e845511474bcbbe70f93744e2288c6bf095914
Yu Gu <crispherg@gmail.com>
2018-12-03 19:09:39 -0500

    added test task list

---
+++ client/src/resource/tasks_pygen_grouped_test.js
@@ -0,0 +1,40 @@
+export const images = [
+"https://crispher.github.io/pages/s21.jpg",
+"https://crispher.github.io/pages/s22.png",
+"https://crispher.github.io/pages/s23.jpg",
+"https://crispher.github.io/pages/s24.jpg",
+"https://crispher.github.io/pages/s25.png",
+"https://crispher.github.io/pages/s26.jpg",
+"https://crispher.github.io/pages/black.jpg",
+]
+export const taskA = [{
+"id":"sample-6-0-0-None",
+"image":6,
+"info":"This is a sample task, you may move your mouse around. Remember to set page zoom to 100% and run your brower in full screen mode. You should see the target at the certer of the screen. No pointer enhancement.",
+"setting":"None",
+"start":{ x:0.401000, y:0.024500},
+"target":{x:0.500000, y:0.500000}
+}
+,
+]
+export const taskB = [{
+"id":"sample-6-0-0-Ctrl",
+"image":6,
+"info":"This is a sample task, you may move your mouse around. Remember to set page zoom to 100% and run your brower in full screen mode. You should see the target at the certer of the screen. Try pressing Ctrl.",
+"setting":"Ctrl",
+"start":{ x:0.401000, y:0.024500},
+"target":{x:0.500000, y:0.500000}
+}
+,
+]
+export const taskC = [{
+"id":"sample-6-0-0-Shake",
+"image":6,
+"info":"This is a sample task, you may move your mouse around. Remember to set page zoom to 100% and run your brower in full screen mode. You should see the target at the certer of the screen. Try shaking your cursor.",
+"setting":"Shake",
+"start":{ x:0.401000, y:0.024500},
+"target":{x:0.500000, y:0.500000}
+}
+,
+]
+export const tasks = [taskA, taskB, taskC]
