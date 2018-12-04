0269f87137818e39c506961cc450ee1120439732
Yu Gu <crispherg@gmail.com>
2018-12-03 19:49:11 -0500

    updated test task list

--- client/src/resource/tasks_pygen_grouped_test.js
+++ client/src/resource/tasks_pygen_grouped_test.js
@@ -12,8 +12,44 @@ export const taskA = [{
 "image":6,
 "info":"This is a sample task, you may move your mouse around. Remember to set page zoom to 100% and run your brower in full screen mode. You should see the target at the certer of the screen. No pointer enhancement.",
 "setting":"None",
-"start":{ x:0.401000, y:0.024500},
+"start":{ x:0.089500, y:0.483000},
 "target":{x:0.500000, y:0.500000}
+}
+,
+{
+"id":"0-0-1-None",
+"image":0,
+"info":"There is no cursor enhancement in this task",
+"setting":"None",
+"start":{ x:0.111500, y:0.538500},
+"target":{x:0.400000, y:0.500000}
+}
+,
+{
+"id":"0-1-1-None",
+"image":0,
+"info":"There is no cursor enhancement in this task",
+"setting":"None",
+"start":{ x:0.952000, y:0.599500},
+"target":{x:0.400000, y:0.500000}
+}
+,
+{
+"id":"0-0-0-None",
+"image":0,
+"info":"There is no cursor enhancement in this task",
+"setting":"None",
+"start":{ x:0.171000, y:0.131000},
+"target":{x:0.400000, y:0.500000}
+}
+,
+{
+"id":"0-1-0-None",
+"image":0,
+"info":"There is no cursor enhancement in this task",
+"setting":"None",
+"start":{ x:0.854000, y:0.146000},
+"target":{x:0.400000, y:0.500000}
 }
 ,
 ]
@@ -22,8 +58,44 @@ export const taskB = [{
 "image":6,
 "info":"This is a sample task, you may move your mouse around. Remember to set page zoom to 100% and run your brower in full screen mode. You should see the target at the certer of the screen. Try pressing Ctrl.",
 "setting":"Ctrl",
-"start":{ x:0.401000, y:0.024500},
+"start":{ x:0.089500, y:0.483000},
 "target":{x:0.500000, y:0.500000}
+}
+,
+{
+"id":"0-1-0-Ctrl",
+"image":0,
+"info":"You may press Ctrl to magnify your cursor",
+"setting":"Ctrl",
+"start":{ x:0.854000, y:0.146000},
+"target":{x:0.400000, y:0.500000}
+}
+,
+{
+"id":"0-1-1-Ctrl",
+"image":0,
+"info":"You may press Ctrl to magnify your cursor",
+"setting":"Ctrl",
+"start":{ x:0.952000, y:0.599500},
+"target":{x:0.400000, y:0.500000}
+}
+,
+{
+"id":"0-0-0-Ctrl",
+"image":0,
+"info":"You may press Ctrl to magnify your cursor",
+"setting":"Ctrl",
+"start":{ x:0.171000, y:0.131000},
+"target":{x:0.400000, y:0.500000}
+}
+,
+{
+"id":"0-0-1-Ctrl",
+"image":0,
+"info":"You may press Ctrl to magnify your cursor",
+"setting":"Ctrl",
+"start":{ x:0.111500, y:0.538500},
+"target":{x:0.400000, y:0.500000}
 }
 ,
 ]
@@ -32,8 +104,44 @@ export const taskC = [{
 "image":6,
 "info":"This is a sample task, you may move your mouse around. Remember to set page zoom to 100% and run your brower in full screen mode. You should see the target at the certer of the screen. Try shaking your cursor.",
 "setting":"Shake",
-"start":{ x:0.401000, y:0.024500},
+"start":{ x:0.089500, y:0.483000},
 "target":{x:0.500000, y:0.500000}
+}
+,
+{
+"id":"0-0-1-Shake",
+"image":0,
+"info":"When you shake your cursor, it is magnified",
+"setting":"Shake",
+"start":{ x:0.111500, y:0.538500},
+"target":{x:0.400000, y:0.500000}
+}
+,
+{
+"id":"0-1-0-Shake",
+"image":0,
+"info":"When you shake your cursor, it is magnified",
+"setting":"Shake",
+"start":{ x:0.854000, y:0.146000},
+"target":{x:0.400000, y:0.500000}
+}
+,
+{
+"id":"0-0-0-Shake",
+"image":0,
+"info":"When you shake your cursor, it is magnified",
+"setting":"Shake",
+"start":{ x:0.171000, y:0.131000},
+"target":{x:0.400000, y:0.500000}
+}
+,
+{
+"id":"0-1-1-Shake",
+"image":0,
+"info":"When you shake your cursor, it is magnified",
+"setting":"Shake",
+"start":{ x:0.952000, y:0.599500},
+"target":{x:0.400000, y:0.500000}
 }
 ,
 ]
