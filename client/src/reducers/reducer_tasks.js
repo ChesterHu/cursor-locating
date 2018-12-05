58019b09b8339291a657d5ba9be77edbb5966dbf
Chufeng <huchufeng@g.ucla.edu>
2018-12-04 21:52:25 -0500

    remove test code

--- client/src/reducers/reducer_tasks.js
+++ client/src/reducers/reducer_tasks.js
@@ -13,6 +13,5 @@ const order = orders[Math.floor(Math.random() * orders.length)];
 const task = [tasks[order[0]], tasks[order[1]], tasks[order[2]]];
 
 export default function() {
-	const temp = [...task[0], ...task[1], ...task[2]];
 	return [...task[0], ...task[1], ...task[2]];
 }
